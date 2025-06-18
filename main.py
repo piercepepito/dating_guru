911a0fd45605421deab2f4268829ae3a462c9ef6
Pierce Pepito <piercepepito@MacBook-Pro-von-Pierce.local>
2025-06-18 14:21:37 +0800

    updated workflow

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 from langchain_openai import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
-from langchain.memory import ConversationBufferMemory
-from langchain_redis import RedisChatMessageHistory
-from langchain.chains import ConversationChain
+from langchain.chains import ConversationalRetrievalChain
+from langchain.prompts import PromptTemplate
+
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import OpenAIEmbeddings
 
 from dotenv import load_dotenv
 import yaml
+import os
 
-import uuid
+from utils import tools
 
 #load environment variables
 load_dotenv()
+
+TRACKING_FILE = "processed_files.json"
+DATA_DIR = "files"
+VECTORSTORE_DIR = "faiss_index"
+
+print('Finding and Syncing new files')
+tools.sync_new_files_to_faiss(DATA_DIR, TRACKING_FILE, VECTORSTORE_DIR)
+
+#Create the retriever
+embedding = OpenAIEmbeddings()
+if os.path.exists(VECTORSTORE_DIR):
+    vectorstore = FAISS.load_local(
+        VECTORSTORE_DIR,
+        embedding,
+        allow_dangerous_deserialization=True
+    )
+else:
+    print('Vectorstore not found')
+
+retriever = vectorstore.as_retriever()
+
 #load yaml file
 with open('utils/prompts.yaml', 'r') as file:
     prompts = yaml.safe_load(file)
@@ -19,41 +42,15 @@ with open('utils/prompts.yaml', 'r') as file:
 llm = ChatOpenAI(temperature = 0.7)
 
 #create a chatbot
-session_id = str(uuid.uuid4())
-history = RedisChatMessageHistory(
-    session_id = session_id,
-    redis_url="redis://localhost:6379"
-)
+history = tools.redis_session("redis://localhost:6379")
+memory = tools.create_chatbot_memory(history)
 
-memory = ConversationBufferMemory(
-    chat_history = history,
-    return_messages = True
-)
-prompt = ChatPromptTemplate([
-    ('system', prompts['chatbot']['system']),
-    ('human', '{history}'),
-    ('human', prompts['chatbot']['human'])
-])
-conversation = ConversationChain(
-    llm = llm,
-    prompt = prompt,
-    memory = memory,
-    verbose = True
-)
+dating_prompt = PromptTemplate.from_template(prompts['chatbot']['human'])
+
+conversation = ConversationalRetrievalChain.from_llm(llm=llm, 
+                                                     retriever=retriever, 
+                                                     memory=memory, 
+                                                     condense_question_prompt = dating_prompt)
 
 #create a chat
-def chat_with_memory():
-    print("Type in 'exit' or 'quit' to stop chatbot.")
-    try:
-        while True:     
-            input_text = input('You: ')
-            if input_text.lower() == 'exit' or input_text.lower() == 'quit': break
-            #if input_text.lower() in ['exit', 'quit']: break
-            response = conversation.invoke({'input': input_text})
-            print(f'Dating Guru: {response['response']}')
-    except KeyboardInterrupt: 
-        print('Goodbye!')
-    finally:
-        history.redis_client.close()
-
-chat_with_memory()
+tools.chat_with_memory(conversation, history)
